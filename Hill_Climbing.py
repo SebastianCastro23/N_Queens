08f7e62a9dbf1981f92d2f977d0b3172c305fbb7
Sebastian Castro <jcastropa@unal.edu.co>
2022-11-30 16:34:06 -0500

    Hill climbing first version

--- Hill_Climbing.py
+++ Hill_Climbing.py
@@ -10,9 +10,6 @@ class state:
         self.queens = queens
         self.n = n
         self.matrix = self.matrix_generation()
-        self.conflicts = 0                                           # Conflict's number always start at 0
-        self.conflict_count()
-        self.conflicts = int(self.conflicts/2)                       # The real number of conflicts because every conflict is counted twice
         
     def matrix_generation(self):                # Generating the state matrix
         matrix = []
@@ -21,57 +18,26 @@ class state:
         for q in queens:
             matrix[q[0]][q[1]] = 1
         return matrix
-    
-    def conflict_count(self):
-        for q in queens:
-            for x in range(0,n):                                                # checks only the rows that are different from the queen's row
-                if x!= q[0]:
-                    if self.matrix[x][q[1]] == 1:                               # checks the queen's column
-                        self.conflicts+=1
-                    topleft_diag_index = q[1]-(q[0]-x)                          # top-left diagonal index in row x
-                    bottomleft_diag_index = q[1]+(q[0]-x)                       # bottom-left diagonal index in row x
-                    if topleft_diag_index >= 0 and topleft_diag_index<n:
-                        if self.matrix[x][topleft_diag_index] == 1:             # checks the top-left diagonal
-                            self.conflicts+=1
-                    if bottomleft_diag_index >= 0 and bottomleft_diag_index < n:
-                        if self.matrix[x][bottomleft_diag_index] == 1:          # checks the bottom-left diagonal
-                            self.conflicts+=1
                     
 
-def subsequential_states(c_state, n):
-    subsequential_states = []
-    
+def move_queen (q):
+    new_queens = []
     for x in range(0,n):
-        current_column = c_state.queens[x][1]
-        new_q1 = (x,(current_column+1)%n)
-        new_q2 = (x,(current_column+2)%n)
-        new_q3 = (x,(current_column+3)%n)
-
-        news1_queens = [0]*n
-        news2_queens = [0]*n
-        news3_queens = [0]*n
-        
-        for y in range(0,n):
-            if y == x:
-                news1_queens[y] = new_q1
-                news2_queens[y] = new_q2
-                news3_queens[y] = new_q3
-            else:
-                news1_queens[y] = c_state.queens[y]
-                news2_queens[y] = c_state.queens[y]
-                news3_queens[y] = c_state.queens[y]
-        
-        
-        new_s1 = state(news1_queens, n)
-        new_s2 = state(news2_queens, n)
-        new_s3 = state(news3_queens, n)
-        
-        subsequential_states.append(new_s1)
-        subsequential_states.append(new_s2)
-        subsequential_states.append(new_s3)
+        if x != q[1]:
+            new_queens.append((q[0],x))
+    return new_queens
     
-    return subsequential_states
-    
+def conflict_count(queens):
+    conflicts = 0
+    for q in queens:
+        for k in queens:                    # checks every other queen
+            if k != q:
+                if q[1] == k[1]:            # checks queen's column
+                    conflicts += 1
+                if abs(q[0]-k[0]) == abs(q[1]-k[1]):
+                    conflicts += 1
+    return conflicts
+                        
 
 #Genariting randomly the initial state      
 queens = []
@@ -87,17 +53,31 @@ initial_state = state(queens,n)                     # Declaring the initial stat
 
 current_state = initial_state
 
-j = 0
+print(queens)
 
-while j <= 1:
+steps = [queens]
+
+while conflict_count(current_state.queens) != 0:
+    
+    new_states_queens = []
         
-    subsequential = subsequential_states(current_state, n)
-    conflict_values = []
+    for q in current_state.queens:
+        moved_q = move_queen(q)
     
-    for s in subsequential:
-        conflict_values.append(s.conflicts)
+        for i in moved_q:
+            state_queens = []
+            for p in range(0,n):
+                if p != q[0]:
+                    state_queens.append(current_state.queens[p])
+            state_queens.insert(q[0],i)
+            new_states_queens.append(state_queens)
     
-    current_state = subsequential[conflict_values.index(min(conflict_values))]
+    conflicts_number = []
+         
+    for n_queens in new_states_queens:
+        conflicts_number.append(int(conflict_count(n_queens)/2))
     
-    j += 1
+    steps.append(new_states_queens[conflicts_number.index(min(conflicts_number))])
+    current_state.queens = new_states_queens[conflicts_number.index(min(conflicts_number))]
 
+print(len(steps))
