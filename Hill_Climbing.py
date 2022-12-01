f339f4058f87a85e9291ea15fdd87d7a9f24621c
Sebastian Castro <jcastropa@unal.edu.co>
2022-12-01 01:25:13 -0500

    Hill climbing second version
    Second version functioning

--- Hill_Climbing.py
+++ Hill_Climbing.py
@@ -1,8 +1,9 @@
 #Hill climbing algorithm
 
 import random
+import copy
 
-n = 4
+n = 50
 
 # State class
 class state:
@@ -10,6 +11,8 @@ class state:
         self.queens = queens
         self.n = n
         self.matrix = self.matrix_generation()
+        self.columns = self.columns()
+        self.diagonals = self.diagonals()
         
     def matrix_generation(self):                # Generating the state matrix
         matrix = []
@@ -18,6 +21,25 @@ class state:
         for q in queens:
             matrix[q[0]][q[1]] = 1
         return matrix
+    
+    def columns(self):
+        columns = [0]*n
+        for q in queens:
+            columns[q[1]] += 1
+        return columns
+            
+    def diagonals(self):
+        diagonals = []                          # diagonals[0] --> down diagonals
+        diagonals.append([0]*(n+1))             # diagonals[1] --> up diagonals
+        diagonals.append([0]*(n+1))
+        for q in queens:
+            if (q[0]-q[1])+2 <= n:
+                diagonals[0][(q[0]-q[1])+2] += 1    # adds one to the down diagonal
+            if (((q[0]+q[1])-1) >= 0) and (((q[0]+q[1])-1)<=n):
+                diagonals[1][(q[0]+q[1])-1] += 1    # adds one to the up diagonal
+            
+        return diagonals                
+                                
                     
 
 def move_queen (q):                             # this function returns all the posible column positions where we can move a queen
@@ -26,8 +48,9 @@ def move_queen (q):                             # this function returns all the 
         if x != q[1]:
             new_queens.append((q[0],x))
     return new_queens
-    
-def conflict_count(queens):
+
+# Conflict count first version
+def conflict_count_1st(queens):
     conflicts = 0
     for q in queens:
         for k in queens:                                    # checks every other queen
@@ -37,6 +60,21 @@ def conflict_count(queens):
                 if abs(q[0]-k[0]) == abs(q[1]-k[1]):        # checks both queen's diagonals at the same time
                     conflicts += 1
     return conflicts
+
+def conflict_count_2nd(state):
+    conflicts = 0;
+    for q in state.queens:
+        if state.columns[q[1]] > 1:
+            conflicts += (state.columns[q[1]]-1)
+        if ((q[0]-q[1])+2) <= n:
+            if state.diagonals[0][(q[0]-q[1])+2] > 1:
+                conflicts += (state.diagonals[0][(q[0]-q[1])+2]-1)
+        if (((q[0]+q[1])-1) >= 0) and (((q[0]+q[1])-1)<=n):
+            if state.diagonals[1][(q[0]+q[1])-1] > 1:
+                conflicts += (state.diagonals[1][(q[0]+q[1])-1]-1)
+    return conflicts
+
+# Conflict count second version
                         
 
 #Genariting randomly the initial state      
@@ -48,16 +86,39 @@ for x in range (0,n):
     
 initial_state = state(queens,n)                     # Declaring the initial state
 
-
-# First version
-
 current_state = initial_state
-
-print(queens)
 
 steps = [queens]
 
-while conflict_count(current_state.queens) != 0:                        # it keeps running while the number of conflicts is different than 0
+# First version
+
+# while conflict_count_1st(current_state.queens) != 0:                        # it keeps running while the number of conflicts is different than 0
+    
+#     new_states_queens = []                                              # here we store all the posible new state queen's configurations
+        
+#     for q in current_state.queens:                                      # we do this for each one of the n queens
+#         moved_q = move_queen(q)                                         # call the move_queen function
+    
+#         for i in moved_q:                                               # for every queen's possible new position we create a state
+#             state_queens = []
+#             for p in range(0,n):
+#                 if p != q[0]:
+#                     state_queens.append(current_state.queens[p])
+#             state_queens.insert(q[0],i)
+#             new_states_queens.append(state_queens)                      # we store the n queens position
+    
+#     conflicts_number = []
+         
+#     for n_queens in new_states_queens:                                                          # for each queens configuration we calculate the number
+#         conflicts_number.append(int(conflict_count_1st(n_queens)/2))                                # of conflicts and we store them in a list
+    
+#     steps.append(new_states_queens[conflicts_number.index(min(conflicts_number))])
+#     current_state.queens = new_states_queens[conflicts_number.index(min(conflicts_number))]     # we change the queens position of the current state
+#                                                                                                 to the one that causes the less conflicts
+                                                                                                
+# Second Version
+
+while conflict_count_2nd(current_state) != 0:                        # it keeps running while the number of conflicts is different than 0
     
     new_states_queens = []                                              # here we store all the posible new state queen's configurations
         
@@ -74,11 +135,12 @@ while conflict_count(current_state.queens) != 0:                        # it kee
     
     conflicts_number = []
          
-    for n_queens in new_states_queens:                                                          # for each queens configuration we calculate the number
-        conflicts_number.append(int(conflict_count(n_queens)/2))                                # of conflicts and we store them in a list
+    for n_queens in new_states_queens:  
+        new_state = state(n_queens,n) 
+        n_conflicts = copy.deepcopy(conflict_count_2nd(new_state))                                                      
+        conflicts_number.append(n_conflicts/2)                            
     
     steps.append(new_states_queens[conflicts_number.index(min(conflicts_number))])
     current_state.queens = new_states_queens[conflicts_number.index(min(conflicts_number))]     # we change the queens position of the current state
-                                                                                                # to the one that causes the less conflicts
-
-print(len(steps))
+                                                                                                # to the one that causes the less conflict
+    print(len(steps))
